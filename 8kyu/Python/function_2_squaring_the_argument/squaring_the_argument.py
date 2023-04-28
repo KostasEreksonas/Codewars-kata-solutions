a5b63f26714599baf76610f7497feca0cc739732
KostasEreksonas <k.ereksonas@gmail.com>
2023-04-28 18:48:21 +0300

    Solution of squaring the argument kata

---
+++ 8kyu/Python/function_2_squaring_the_argument/squaring_the_argument.py
@@ -0,0 +1,12 @@
+#!/usr/share/bin python3
+
+def square(n):
+    return n*n
+
+num1 = 3
+num2 = 7
+num3 = 12
+
+print(f"Square of {num1} is: {square(num1)}")
+print(f"Square of {num2} is: {square(num2)}")
+print(f"Square of {num3} is: {square(num3)}")
