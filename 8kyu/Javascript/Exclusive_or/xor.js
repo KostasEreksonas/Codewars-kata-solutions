c065763bc27a1cb33692f12a95fad60ddb7ea752
KostasEreksonas <k.ereksonas@gmail.com>
2023-06-14 08:35:12 +0300

    Solution for exclusive or (xor) Logical Operator kata

---
+++ 8kyu/Javascript/Exclusive_or/xor.js
@@ -0,0 +1,12 @@
+function xor(a, b) {
+  if (a === b) {
+    return false;
+  } else {
+    return true;
+  }
+}
+
+console.log(xor(false,false))
+console.log(xor(false,true))
+console.log(xor(true,false))
+console.log(xor(true,true))
