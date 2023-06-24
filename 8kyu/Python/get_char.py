1b55da72106d9458d174b7291d4abcb223cc5361
KostasEreksonas <k.ereksonas@gmail.com>
2023-06-24 09:52:16 +0300

    Solution for get character from ASCII Value kata

---
+++ 8kyu/Python/get_char.py
@@ -0,0 +1,12 @@
+#!/usr/bin/env python3
+
+def get_char(c):
+    return chr(c)
+
+val1=65
+val2=78
+val3=102
+
+print(f"{val1} is {get_char(val1)}")
+print(f"{val2} is {get_char(val2)}")
+print(f"{val3} is {get_char(val3)}")
