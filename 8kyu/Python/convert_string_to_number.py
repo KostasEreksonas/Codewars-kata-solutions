bf60088fb7be133d0fa920fcc3433ce7819465ba
KostasEreksonas <k.ereksonas@gmail.com>
2023-06-27 17:00:57 +0300

    Solution to convert a string to a number kata

---
+++ 8kyu/Python/convert_string_to_number.py
@@ -0,0 +1,15 @@
+#!/usr/bin/env python3
+
+def string_to_number(s):
+    n=int(s)
+    return n
+
+str1="1234"
+str2="605"
+str3="1405"
+str4="-7"
+
+print(f"{str1} is {string_to_number(str1)}")
+print(f"{str2} is {string_to_number(str2)}")
+print(f"{str3} is {string_to_number(str3)}")
+print(f"{str4} is {string_to_number(str4)}")
