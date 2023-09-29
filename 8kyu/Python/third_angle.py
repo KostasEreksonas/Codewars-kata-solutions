8e46fc3bb07c38bd0b83c49f5792c38d2edbe2dd
KostasEreksonas <k.ereksonas@gmail.com>
2023-09-29 16:50:20 +0300

    Solution for Third Angle of a Triangle kata

---
+++ 8kyu/Python/third_angle.py
@@ -0,0 +1,8 @@
+#!/usr/bin/env python3
+
+def other_angle(a, b):
+    return 180-a-b
+
+print(f"Third angle: {other_angle(60,60)} degrees")
+print(f"Third angle: {other_angle(20,30)} degrees")
+print(f"Third angle: {other_angle(45,45)} degrees")
