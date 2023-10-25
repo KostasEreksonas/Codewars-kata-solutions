745f6b3746381aa402c89a229dbfd1856a31d47c
KostasEreksonas <k.ereksonas@gmail.com>
2023-10-25 21:26:05 +0300

    Solution for Volume of a Cuboid kata

---
+++ 8kyu/Python/Volume_of_a_Cuboid.py
@@ -0,0 +1,8 @@
+#!/usr/bin/env python3
+
+def get_volume_of_cuboid(length, width, height):
+    return length*width*height
+
+print(get_volume_of_cuboid(1, 2, 2))
+print(get_volume_of_cuboid(6.3, 2, 5))
+print(get_volume_of_cuboid(2, 3, 4))
