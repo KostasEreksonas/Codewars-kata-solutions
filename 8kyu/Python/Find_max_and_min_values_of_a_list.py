0f1bbb85efcd5352b7abf157c2253b8795047134
KostasEreksonas <k.ereksonas@gmail.com>
2023-11-11 13:40:33 +0200

    Solution for Find Maximum and Minimum Values of a List kata

---
+++ 8kyu/Python/Find_max_and_min_values_of_a_list.py
@@ -0,0 +1,12 @@
+#!/usr/bin/env python3
+
+def minimum(arr):
+    return min(arr)
+
+def maximum(arr):
+    return max(arr)
+
+arr = [-52, 56, 30, 29, -54, 0, -110]
+
+print(minimum(arr))
+print(maximum(arr))
