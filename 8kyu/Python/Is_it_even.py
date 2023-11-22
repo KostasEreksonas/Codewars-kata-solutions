b5b0dc68fb86d592dbf12ce517d0bf769a5f66cf
KostasEreksonas <k.ereksonas@gmail.com>
2023-11-22 20:37:49 +0200

    Solution for Is it even? kata

---
+++ 8kyu/Python/Is_it_even.py
@@ -0,0 +1,10 @@
+#!/usr/bin/env python3
+
+def is_even(n):
+    return True if n % 2 == 0 else False
+
+print(is_even(0))
+print(is_even(0.5))
+print(is_even(1))
+print(is_even(-1))
+print(is_even(2))
