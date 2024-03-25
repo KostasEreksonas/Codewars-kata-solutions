dab206a4c3c8da0ae27e7ec25f878d92668d3f40
KostasEreksonas <k.ereksonas@gmail.com>
2024-03-25 12:19:11 +0200

    Solution for Bin to Decimal kata

---
+++ 8kyu/Python/bin_to_decimal.py
@@ -0,0 +1,11 @@
+#!/usr/bin/env python3
+
+def bin_to_decimal(inp):
+    return int(inp, 2)
+
+print(bin_to_decimal("0"))
+print(bin_to_decimal("1"))
+print(bin_to_decimal("10"))
+print(bin_to_decimal("11"))
+print(bin_to_decimal("101010"))
+print(bin_to_decimal("1010101"))
