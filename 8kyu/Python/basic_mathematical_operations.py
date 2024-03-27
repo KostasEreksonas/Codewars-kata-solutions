fb1c4c11b189e503d1112cbe773863aa6dbbf53f
KostasEreksonas <k.ereksonas@gmail.com>
2024-03-27 09:57:00 +0200

    Solution for Basic Mathematical Operations kata

---
+++ 8kyu/Python/basic_mathematical_operations.py
@@ -0,0 +1,16 @@
+#!/usr/bin/env python3
+
+def basic_op(operator, value1, value2):
+    if operator == '+':
+        return value1 + value2
+    elif operator == "-":
+        return value1 - value2
+    elif operator == "/":
+        return value1 / value2
+    elif operator == "*":
+        return value1 * value2
+
+print(basic_op('+', 4, 7))
+print(basic_op('-', 15, 18))
+print(basic_op('*', 5, 5))
+print(basic_op('/', 49, 7))
